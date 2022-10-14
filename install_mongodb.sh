24f67585b77c6c9eec05618e54e19da7f38338ee
sweeft <kuchinskiy96@gmail.com>
2022-10-14 10:08:21 +0000

    Homework 6

---
+++ install_mongodb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
+
