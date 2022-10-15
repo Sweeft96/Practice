a5d75f985e8d54e175cbb79dd5d1fddd3fe776a8
sweeft <kuchinskiy96@gmail.com>
2022-10-15 05:30:36 +0000

    Fixed script

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 sudo apt update
 sudo apt install -y mongodb-org
 sudo systemctl start mongod
