f96e768f260651cda5230e355da3f7c273115e24
Danil Kuchinskiy <kuchinskiy96@gmail.com>
2022-10-24 12:44:33 +0600

    Update

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,13 +1,13 @@
 provider "google" {
 version = "3.17.0"
-project = "${var.project}"
-region = "${var.region}"
+project = "infra-365211"
+region = "europe-west1"
 }
 module "storage-bucket" {
 source = "SweetOps/storage-bucket/google"
 version = "0.1.1"
 # Имена поменяйте на другие
-name = ["storage-bucket-test", "storage-bucket-test2"]
+name = ["storage-bucket-infra365211", "storage-bucket-infra365212"]
 }
 output storage-bucket_url {
 value = "${module.storage-bucket.url}"
