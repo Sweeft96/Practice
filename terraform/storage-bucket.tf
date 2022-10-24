4c1aea723577c359e81d7141790e053fd251cf03
Sweeft96 <63405575+Sweeft96@users.noreply.github.com>
2022-10-24 17:55:05 +0600

    Update storage-bucket.tf

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,22 +1,4 @@
 provider "google" {
-
-  version = "3.17"
-  project = "infra-365211"
-  region  = "europe-west1"
-}
-
-module "storage-bucket" {
-  source  = "SweetOps/storage-bucket/google"
-  version = "0.3.1"
-
-  # Имя поменяйте на другое
-  name = "infra-365211"
-}
-
-output storage-bucket_url {
-  value = module.storage-bucket.url
-}
-
 version = "3.17.0"
 project = "${var.project}"
 region = "${var.region}"
