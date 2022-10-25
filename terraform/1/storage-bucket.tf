22d86b864f695228d8a857651e712970722ae965
Danil Kuchinskiy <kuchinskiy96@gmail.com>
2022-10-25 14:44:14 +0600

    New config storage

--- terraform/1/storage-bucket.tf
+++ terraform/1/storage-bucket.tf
@@ -1,17 +1,16 @@
 provider "google" {
 
-  version = "3.17.0"
   project = "${var.project}"
   region  = "${var.region}"
 }
 module "storage-bucket" {
-  source  = "SweetOps/storage-bucket/google"
-  version = "0.3.2"
+  #source  = "SweetOps/storage-bucket/google"
+  #version = "0.3.2"
   #name = "storage-bucket-test-44543534646"
-  #source      = "git::https://github.com/SweetOps/terraform-google-storage-bucket.git?ref=master"
+  source      = "git::https://github.com/SweetOps/terraform-google-storage-bucket.git?ref=master"
   name        = "sdfsadaafaffads"
-  #stage       = "production"
-  #namespace   = "sweetops"
+  stage       = "production"
+  namespace   = "sweetops"
   location    = "europe-west1"
 }
 output storage-bucket_url {
